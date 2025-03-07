ac4c9398d3146ebb230c320ae7eadeaeb2eecbff
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-07 10:20:23 +0300

    Override equels and hshCode

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,14 @@ public class Main {
     public static void main(String[] args) {
 
         Book book1 = new Book("Den Brawn","Code Da'Vinci");
-        Book book2 = new Book("Alex Hersh","Gravity Falls");
-        Book book3 = new Book("Poll Garden","How i grown potato");
 
         Ticket tik1 = new Ticket(1123);
-        Ticket tik2 = new Ticket(4567);
-        Ticket tik3 = new Ticket(8901);
+        Ticket tik1_copy = new Ticket(1123);
 
         Map<Ticket, Book> library = new HashMap<>();
         library.put(tik1,book1);
-        library.put(tik2,book2);
-        library.put(tik3,book3);
 
-        System.out.println(library.get(tik1).toString());
+        System.out.println(library.get(tik1_copy).toString());
 
     }
 }
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,8 +1,23 @@
+import java.util.Objects;
+
 public class Ticket {
 
     int number;
     Ticket(int n){
         this.number = n;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return number == ticket.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+
 
 }
